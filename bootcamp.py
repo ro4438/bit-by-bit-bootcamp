3c7bda676b4f124d01c143d741faa10865e41163
ro4438 <37229913+ro4438@users.noreply.github.com>
2020-04-27 18:42:08 -0500

    Update bootcamp.py

--- bootcamp.py
+++ bootcamp.py
@@ -1,3 +1,4 @@
+#binary conversion function and call
 decimal_accuracy = 7
 
 def decimal_to_binary(num):  # Function inputs a float value and returns a binary equivalent
@@ -7,9 +8,9 @@ def decimal_to_binary(num):  # Function inputs a float value and returns a binar
 
     decimal = round(num % 1, decimal_accuracy)  # Extract fractional number part of decimal
     w_num = int(num)  # Extract whole number part of decimal.
+    i = 0
 
-    i = 0  # Some fractional decimal numbers have infinite binary values, so we limit this loop below.
-
+    # Loop to find binary of fractional part.
     while (decimal != 1 and i < decimal_accuracy):
         decimal = decimal * 2
         fractional.append(int(decimal // 1))
@@ -17,15 +18,23 @@ def decimal_to_binary(num):  # Function inputs a float value and returns a binar
         if (decimal == 0): break  # Removes trailing zeros.
         i = i + 1
 
-        # Loop to find binary of whole number part.
+    # Loop to find binary of whole number part.
     while (w_num != 0):
         whole.append(w_num % 2)
         w_num = w_num // 2
     whole.reverse()
 
-    return str(whole + fractional) 
-
+    final_value = float("".join(map(str, whole+fractional))) 
+    if final_value.is_integer():
+      return int(final_value)
+    else: 
+      return final_value
 # Converts user input to float which is a string initially
 number = float(input("Enter Any base-10 Number: "))
 print("The Binary Equivalant: ", decimal_to_binary(number))
+print("----------------------")
+
+
+
+
 print("Done")
