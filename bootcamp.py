8ea7a63bc7d7a2900f20ef6742329be20aa18341
ro4438 <37229913+ro4438@users.noreply.github.com>
2020-04-27 19:23:25 -0500

    Update bootcamp.py

--- bootcamp.py
+++ bootcamp.py
@@ -31,7 +31,7 @@ def decimal_to_binary(num):  # Function inputs a float value and returns a binar
       return final_value
 # Converts user input to float which is a string initially
 number = float(input("Enter Any base-10 Number: "))
-print("The Binary Equivalant: ", decimal_to_binary(number))
+print("The Binary Equivalent: ", decimal_to_binary(number))
 print("----------------------")
 
 #get value at specific location in Pascal's Triangle
@@ -51,7 +51,6 @@ def pascal_triangle(lineNumber):
         i = i + 1
     return list1
 
-
 def binomial_coef(n, k):
     pascalTriangle = pascal_triangle(n)
     return (pascalTriangle[n][k - 1])
@@ -61,70 +60,15 @@ element_number = int(input("Enter Element Number For Designated Row (Must be les
 print(f"Row: {row_number}, Element: {element_number} - Value: {binomial_coef(row_number, element_number)}")
 print("----------------------")
 
-import random
-import simplegui
-
-def new_game():
-    global card3, po, state, exposed, card1
-
-    def create(card):
-        while len(card) != 8:
-            num = random.randrange(0, 8)
-            if num not in card:
-                card.append(num)
-        return card
-
-    card3 = []
-    card1 = []
-    card2 = []
-    po = []
-    card1 = create(card1)
-    card2 = create(card2)
-    card1.extend(card2)
-    random.shuffle(card1)
-    state = 0
-    exposed = []
-    for i in range(0, 16, 1):
-        exposed.insert(i, False)
-
-
-def mouseclick(pos):
-    global card3, po, state, exposed, card1
-    if state == 2:
-        if card3[0] != card3[1]:
-            exposed[po[0]] = False
-            exposed[po[1]] = False
-        card3 = []
-        state = 0
-        po = []
-    ind = pos[0] // 50
-    card3.append(card1[ind])
-    po.append(ind)
-    if exposed[ind] == False and state < 2:
-        exposed[ind] = True
-        state += 1
-
-
-def draw(canvas):
-    global card1
-    gap = 0
-    for i in range(0, 16, 1):
-        if exposed[i] == False:
-            canvas.draw_polygon([[0 + gap, 0], [0 + gap, 100], [50 + gap, 100], [50 + gap, 0]], 1, "Black", "Green")
-        elif exposed[i] == True:
-            canvas.draw_text(str(card1[i]), [15 + gap, 65], 50, 'White')
-        gap += 50
-
-
-frame = simplegui.create_frame("Memory", 800, 100)
-frame.add_button("Reset", new_game)
-label = frame.add_label("Turns = 0")
-
-frame.set_mouseclick_handler(mouseclick)
-frame.set_draw_handler(draw)
-
-new_game()
-frame.start()
-
+#Pig Latin Converter
+words = str(input("Enter some text to translate to pig latin: "))
+print(f"You entered: {words}")
+words = words.split(' ') #create list of words from string of text
+translated = []
+for i in words:
+    if len(i) >= 3: #I only want to translate words greater than 3 characters
+      i = i[1:] + i[0] + "ay"
+    translated.append(i)
+print("Translated: ", " ".join(translated))
+print("----------------------")
 print("Done")
-
